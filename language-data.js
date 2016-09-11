a3cde1c1538ea2f9eb238e3f63dfe66cef6fe09f
Brian Levine <balevine@github.com>
2016-09-10 23:58:52 -0700

    Create initial language-data.js file

---
+++ language-data.js
@@ -0,0 +1,32 @@
+function person() {
+  var person = [
+    "First Person",
+    "Second Person",
+    "Third Person"
+  ];
+  return person;
+}
+
+function number() {
+    var number = [
+      "Singular",
+      "Plural"
+    ];
+    return number;
+}
+
+function tense() {
+  var tense = [
+    "Present",
+    "Present Progressive",
+    "Past Progressive",
+    "Imperfect",
+    "Simple Past",
+    "Past Perfect",
+    "Future",
+    "Future Perfect",
+    "Conditional",
+    "Conditional Past"
+  ];
+  return tense;
+}
